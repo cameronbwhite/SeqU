dc66ebe7d0fe0cefcb8bb535f89e0fba9d934877
Cameron White <cawhite@pdx.edu>
2013-10-02 14:29:36 -0700

    Compliance Level 1
    - Changed descriptions
    - Added optionals
     * '--format'
     * '--separator'
     * '--equal-width'
    - Added Increment
    - Changed string generation
    - TODO: number format
    - TODO: equal width

--- sequ.py
+++ sequ.py
@@ -2,23 +2,46 @@
 # Cameron Brandon White
 
 import argparse
+from functools import reduce
 
 parser = argparse.ArgumentParser(
-	description='Print the numbers from start to end.')
+	description='Print numbers from FIRST to LAST, in steps of INCREMENT')
 
 parser.add_argument(
-	'start',
-	help='The starting place',
+	'-f', '--format', metavar='FORMAT',
+	help='use printf style floating-point FORMAT',
+	dest='format')
+
+parser.add_argument(
+	'-s', '--separator', metavar='STRING',
+	help='use the STRING to separate numbers',
+	dest='separator',
+	default='\n',
+	type=str)
+
+parser.add_argument(
+	'-w', '--equal-width',
+	help='equalize width by padding with leading zeroes',
+	dest='equalWidth',
+	default=False, const=True, action='store_const')
+
+parser.add_argument(
+	'first', metavar='FIRST',
+	help='The first number',
 	type=int)
 
 parser.add_argument(
-	'end',
-	help='The ending place',
+	'last', metavar='LAST',
+	help='The last number',
 	type=int)
+
+parser.add_argument(
+	'increment', metavar='INCREMENT',
+	type=int, default=1, nargs='?')
 
 if __name__ == '__main__':
 
 	args = parser.parse_args()
 
-	for i in range(args.start, args.end):
-		print(i)
+	print(reduce(lambda x, y: str(x) + args.separator + str(y),\
+		range(args.first, args.last, args.increment)))
