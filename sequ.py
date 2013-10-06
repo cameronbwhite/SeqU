f91414dd20561b0f92b29cce54aea58fe6448298
Cameron White <cawhite@pdx.edu>
2013-10-06 08:50:17 -0700

    Updated to meet CL2
    - added char type
    - added unescape_control_codes
    - added group1
        - changed separator
        - added words
    - added group2
        - added pad
        - added pad-spaces
        - changed equal-width

--- sequ.py
+++ sequ.py
@@ -5,6 +5,19 @@ import argparse
 import codecs
 from functools import reduce
 
+# Function which is used as a type for argparse. If the string does 
+# not contain only one character then error thrown.
+def char(string):
+    string = unescape_control_codes(string)
+    if len(string) == 1:
+        return string
+    raise argparse.ArgumentTypeError('must be a single character')
+
+# Control codes are automatically escaped when passed through
+# the command line. The following removes the escaping.
+def unescape_control_codes(string):
+    return codecs.getdecoder('unicode_escape')(string)[0]
+
 parser = argparse.ArgumentParser(
     description='Print numbers from FIRST to LAST, in steps of INCREMENT')
 
@@ -12,26 +25,46 @@ parser.add_argument(
     '--version',
     action='version', version='2.0')
 
-parser.add_argument(
+group1 = parser.add_mutually_exclusive_group()
+
+group1.add_argument(
     '-s', '--separator', metavar='STRING',
     help='use the STRING to separate numbers',
     dest='separator',
     default='\n',
     type=str)
 
-group = parser.add_mutually_exclusive_group()
+group1.add_argument(
+    '-W', '--words',
+    help='Output the sequence as a single space-separeted line of words',
+    dest='separator',
+    const=' ', action='store_const')
 
-group.add_argument(
+group2 = parser.add_mutually_exclusive_group()
+
+group2.add_argument(
     '-f', '--format', metavar='FORMAT',
     help='use python format style floating-point FORMAT',
     dest='format', 
     type=str)
 
-group.add_argument(
+group2.add_argument(
+    '-p', '--pad', metavar='CHAR',
+    help='equalize width by padding with the padding provided',
+    dest='padding',
+    type=char)
+
+group2.add_argument(
+    '-P', '--pad-spaces',
+    help='equalize width by padding with leading spaces',
+    dest='padding',
+    const=' ', action='store_const')
+
+group2.add_argument(
     '-w', '--equal-width',
     help='equalize width by padding with leading zeroes',
-    dest='equalWidth',
-    default=False, const=True, action='store_const')
+    dest='padding',
+    const='0', action='store_const')
 
 parser.add_argument(
     'first', metavar='FIRST',
@@ -58,18 +91,18 @@ if __name__ == '__main__':
         # If the format option was used then the format given will 
         # be passed directly.
         format = '{{{}}}'.format(args.format)
-    elif args.equalWidth:
-        # If the equalWidth option was used then the format will
-        # pad 0s using the length of the largest number.
-        format = '{{:0{}}}'.format(len(str(args.last)))
+    elif args.padding:
+        # If a padding was provided then apply it by constructing
+        # the format with the supplied padding character and the
+        # length of the largest number.
+        format = '{{:{}>{}d}}'.format(args.padding, 
+                                      len(str(args.last)))
     else:
         # Else the empty format will be used specifing no 
         # formatting.
         format = '{}'
 
-    # Control codes are automatically escaped when passed through
-    # the command line. The following statement removes the escaping.
-    separator = codecs.getdecoder('unicode_escape')(args.separator)[0]
+    separator = unescape_control_codes(args.separator)
 
     # The following statement creates a list of integers using the 
     # range specified by first, last, and increment. The map 
