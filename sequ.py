2e954588795bc0a80bbe72e8ab078cec21239c40
Cameron White <cawhite@pdx.edu>
2013-10-15 21:58:15 -0700

    Updated code format of sequ.py

--- sequ.py
+++ sequ.py
@@ -5,7 +5,7 @@ import argparse
 import codecs
 from functools import reduce
 
-formatWords = ['arabic', 'ARABIC',
+FORMAT_WORDS = ['arabic', 'ARABIC',
                'alpha',  'ALPHA',
                'roman',  'ROMAN',
                'floating']
@@ -73,7 +73,7 @@ def char(string):
 # Function which is used as a type for argparse. If the string is
 # not in list then an error is thrown.
 def formatWord(word):
-    if word in formatWords:
+    if word in FORMAT_WORDS:
         return word
     else:
         raise argparse.ArgumentTypeError('Must be a format Word')
@@ -91,101 +91,105 @@ def srange(formatWord, first, last, increment):
     return mapping
 
 
-parser = argparse.ArgumentParser(
+PARSER = argparse.ArgumentParser(
     description='Print numbers from FIRST to LAST, in steps of INCREMENT')
 
-parser.add_argument(
+PARSER.add_argument(
     '--version',
     action='version', version='2.0')
 
-parser.add_argument(
-   '-F', '--format-word', 
+PARSER.add_argument(
+   '-F', '--format-word',
    type=formatWord)
 
-group1 = parser.add_mutually_exclusive_group()
+GROUP1 = PARSER.add_mutually_exclusive_group()
 
-group1.add_argument(
+GROUP1.add_argument(
     '-s', '--separator', metavar='STRING',
     help='use the STRING to separate numbers',
     dest='separator',
     default='\n',
     type=str)
 
-group1.add_argument(
+GROUP1.add_argument(
     '-W', '--words',
     help='Output the sequence as a single space-separeted line of words',
     dest='separator',
     const=' ', action='store_const')
 
-group2 = parser.add_mutually_exclusive_group()
+GROUP2 = PARSER.add_mutually_exclusive_group()
 
-group2.add_argument(
+GROUP2.add_argument(
     '-f', '--format', metavar='FORMAT',
     help='use python format style floating-point FORMAT',
-    dest='format', 
+    dest='format',
     type=str)
 
-group2.add_argument(
+GROUP2.add_argument(
     '-p', '--pad', metavar='CHAR',
     help='equalize width by padding with the padding provided',
     dest='padding',
     type=char)
 
-group2.add_argument(
+GROUP2.add_argument(
     '-P', '--pad-spaces',
     help='equalize width by padding with leading spaces',
     dest='padding',
     const=' ', action='store_const')
 
-group2.add_argument(
+GROUP2.add_argument(
     '-w', '--equal-width',
     help='equalize width by padding with leading zeroes',
     dest='padding',
     const='0', action='store_const')
 
-parser.add_argument(
+GROUP2.add_argument(
     'first', metavar='FIRST',
     help='The first number',
     type=int)
 
-parser.add_argument(
+GROUP2.add_argument(
     'last', metavar='LAST',
     help='The last number',
     type=int)
 
-parser.add_argument(
+PARSER.add_argument(
     'increment', metavar='INCREMENT',
     help='The step size',
     type=int, default=1, nargs='?')
 
-if __name__ == '__main__':
+def main():
 
-    args = parser.parse_args()
+    args = PARSER.parse_args()
 
     # Depending on the options used the format will be constructed
-    # differently. 
-    if args.format:
-        # If the format option was used then the format given will 
+    # differently.
+    if args.formatStr:
+        # If the format option was used then the format given will
         # be passed directly.
-        format = '{{{}}}'.format(args.format)
+        formatStr = '{{{}}}'.format(args.formatStr)
     elif args.padding:
         # If a padding was provided then apply it by constructing
         # the format with the supplied padding character and the
         # length of the largest number.
-        format = '{{:{}>{}}}'.format(args.padding, 
+        formatStr = '{{:{}>{}}}'.format(args.padding,
                                      len(str(args.last)))
     else:
-        # Else the empty format will be used specifing no 
+        # Else the empty format will be used specifing no
         # formatting.
-        format = '{}'
+        formatStr = '{}'
 
     separator = unescape_control_codes(args.separator)
 
-    # The following statement creates a list of integers using the 
-    # range specified by first, last, and increment. The map 
+    # The following statement creates a list of integers using the
+    # range specified by first, last, and increment. The map
     # transform the list into a list of interger strings using the
     # format given. The reduce concatenates all the strings in the
     # list together with the separator in between.
     print(reduce(lambda x, y: x + separator + y,
-        map(lambda a: format.format(a),
+        map(lambda a: formatStr.format(a),
             range(args.first, args.last + 1, args.increment))))
+
+if __name__ == '__main__':
+
+    main()
