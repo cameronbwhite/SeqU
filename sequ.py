4df9bea00375a0369d6293878640f1679a03b71c
Cameron White <cawhite@pdx.edu>
2013-11-07 18:53:45 -0800

    Style

--- sequ.py
+++ sequ.py
@@ -10,20 +10,16 @@ import argparse
 import codecs
 import sys
 
-# Function which is used as a type for argparse. If the string does 
-# not contain only one character then error thrown.
 def char(string):
+    """ Function which is used as a type for argparse. If the
+    string does not contain only one character then error
+    thrown. """
     string = unescape_control_codes(string)
     if len(string) == 1:
         return string
     raise argparse.ArgumentTypeError('must be a single character')
 
-# Control codes are automatically escaped when passed through
-# the command line. The following removes the escaping.
-def unescape_control_codes(string):
-    return codecs.getdecoder('unicode_escape')(string)[0]
-
-def floatIntType(value):
+def float_int_type(value):
     """ Function which is used as a type for argparse.
     If the argument is not a valid float or int an
     error is thrown. The type returned will be int if
@@ -37,7 +33,7 @@ def floatIntType(value):
         return value_type(value)
     except ValueError:
         raise argparse.ArgumentTypeError('must be a valid float or int')
-    
+
 PARSER = argparse.ArgumentParser(
     description='Print numbers from FIRST to LAST, in steps of INCREMENT')
 
@@ -45,42 +41,42 @@ PARSER.add_argument(
     '--version',
     action='version', version='2.0')
 
-group1 = PARSER.add_mutually_exclusive_group()
+GROUP1 = PARSER.add_mutually_exclusive_group()
 
-group1.add_argument(
+GROUP1.add_argument(
     '-s', '--separator', metavar='STRING',
     help='use the STRING to separate numbers',
     dest='separator',
     default='\n',
     type=str)
 
-group1.add_argument(
+GROUP1.add_argument(
     '-W', '--words',
     help='Output the sequence as a single space-separeted line of words',
     dest='separator',
     const=' ', action='store_const')
 
-group2 = PARSER.add_mutually_exclusive_group()
+GROUP2 = PARSER.add_mutually_exclusive_group()
 
-group2.add_argument(
+GROUP2.add_argument(
     '-f', '--format', metavar='FORMAT',
     help='use python format style floating-point FORMAT',
     dest='format_str',
     type=str)
 
-group2.add_argument(
+GROUP2.add_argument(
     '-p', '--pad', metavar='CHAR',
     help='equalize width by padding with the padding provided',
     dest='padding',
     type=char)
 
-group2.add_argument(
+GROUP2.add_argument(
     '-P', '--pad-spaces',
     help='equalize width by padding with leading spaces',
     dest='padding',
     const=' ', action='store_const')
 
-group2.add_argument(
+GROUP2.add_argument(
     '-w', '--equal-width',
     help='equalize width by padding with leading zeroes',
     dest='padding',
@@ -89,17 +85,17 @@ group2.add_argument(
 PARSER.add_argument(
     'first', metavar='FIRST',
     help='The first number',
-    type=floatIntType, default=1, nargs='?')
+    type=float_int_type, default=1, nargs='?')
 
 PARSER.add_argument(
     'increment', metavar='INCREMENT',
     help='The step size',
-    type=floatIntType, default=1, nargs='?')
+    type=float_int_type, default=1, nargs='?')
 
 PARSER.add_argument(
     'last', metavar='LAST',
     help='The last number',
-    type=floatIntType)
+    type=float_int_type)
 
 def frange(start, stop, step=1):
     """A range function that accepts floats"""
@@ -111,7 +107,7 @@ def frange(start, stop, step=1):
 def separate(separator, iterable):
     """ seperate generates a iterable with the
     separator element between every element of the
-    given iterable """ 
+    given iterable. """
     it = iter(iterable)
     value = next(it)
     yield value
@@ -119,23 +115,29 @@ def separate(separator, iterable):
         yield separator
         yield i
 
+def unescape_control_codes(string):
+    """ Control codes are automatically escaped when passed
+    through the command line. The following removes the
+    escaping. """
+    return codecs.getdecoder('unicode_escape')(string)[0]
+
 def main():
 
     args = PARSER.parse_args()
     
-    # Determine the length of the largest fractional part of the 
+    # Determine the length of the largest fractional part of the
     # three positional arguments. From bottom to top, right to left
     # this statement does the following. First the number is turned
     # into a string then split into its integer and fractional parts,
-    # next if the number had a fractional part the len of it is 
+    # next if the number had a fractional part the len of it is
     # taken, finally the largest length is return.
     fractional_length = max(
         map(lambda parts: len(parts[1]) if len(parts) == 2 else 0,
             map(lambda number: str(number).split('.'), 
                 [args.first, args.last, args.increment])))
-    
+
     # The length of the largest number.
-    max_length = len(str(int(args.last))) + fractional_length 
+    max_length = len(str(int(args.last))) + fractional_length
     max_length += 1 if fractional_length else 0 # for the '.'
 
     # Depending on the options used the format will be constructed
@@ -163,7 +165,7 @@ def main():
     for i in separate(separator, map(lambda a: format_str.format(a),
             frange(args.first, args.last + 1, args.increment))):
         print(i, end='')
-    
+
     print()
 
 if __name__ == '__main__':
